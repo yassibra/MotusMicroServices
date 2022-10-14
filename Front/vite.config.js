36cef6626de43de89bf6a1a86d766304a9ed94df
yassibra <tahriyassinepro@gmail.com>
2022-10-14 22:22:17 +0200

    docker

--- Front/vite.config.js
+++ Front/vite.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
+  server:{
+    host: '0.0.0.0'
+  },
   plugins: [svelte()],
 });
